9902328ee66b9001bd6ceadcecf1c022d1fa3ed2
dief375 <dief375@yahoo.com>
2025-03-19 14:12:38 +0200

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "API is running!"
+# Database connection
+def get_db_connection():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME"),
+    )
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    return jsonify({"message": "API is running..."})
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    
+    cursor.execute("SELECT * FROM products")
+    products = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+    
+    return jsonify(products)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
