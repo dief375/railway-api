4caed3d4369f5f0f314f583b50ad1d27813b9eba
dief375 <dief375@yahoo.com>
2025-03-20 10:26:54 +0200

    app.py

--- app.py
+++ app.py
@@ -25,16 +25,23 @@ def home():
 
 @app.route("/Users", methods=["GET"])
 def get_products():
-    conn = get_db_connection()
-    cursor = conn.cursor(dictionary=True)
-    
-    cursor.execute("SELECT * FROM Users")
-    products = cursor.fetchall()
-    
-    cursor.close()
-    conn.close()
-    
-    return jsonify(products)
+   try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        
+        cursor.execute("SELECT * FROM Users")
+        users = cursor.fetchall()
+        
+        print("Fetched users:", users)  # Debugging print
+        
+        cursor.close()
+        conn.close()
+        
+        return jsonify(users)
+
+    except mysql.connector.Error as err:
+        print("Database error:", err)
+        return jsonify({"error": str(err)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
